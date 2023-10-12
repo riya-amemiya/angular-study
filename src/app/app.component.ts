6984ddc78a90ba683d6c112cfc16eeb7a071df89
Yuta Nishi <riyaamemiya@gmail.com>
2023-10-12 18:00:46 +0900

    fix(src): いくつかのコンポーネントを修正し、リストアイテムを追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'my-app-angular';
+  title = "my-app-angular";
+  items = [
+    {
+      name: "item 1",
+      description: "description 1",
+    },
+    {
+      name: "item 2",
+      description: "description 2",
+    },
+    {
+      name: "item 3",
+      description: "description 3",
+    },
+  ];
 }
