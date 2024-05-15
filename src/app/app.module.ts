d90b156f6ad326d0ec827547e8e43cbb5e8f1edb
Yuta Nishi <riyaamemiya@gmail.com>
2024-05-15 15:20:22 +0900

    refactor: biome.jsonのアップデートとpackage.jsonの依存関係を更新

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ItemsComponent } from './items/items.component';
+import { ItemsComponent } from "./items/items.component";
 
 @NgModule({
   declarations: [AppComponent, ItemsComponent],
--- src/app/items/items.component.spec.ts
+++ src/app/items/items.component.spec.ts
@@ -1,21 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ItemsComponent } from './items.component';
+import { ItemsComponent } from "./items.component";
 
-describe('ItemsComponent', () => {
+describe("ItemsComponent", () => {
   let component: ItemsComponent;
   let fixture: ComponentFixture<ItemsComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ItemsComponent]
+      declarations: [ItemsComponent],
     });
     fixture = TestBed.createComponent(ItemsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
